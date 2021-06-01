aa654e78e098b00d5a44d39922ba24c27505150c
akyravish <info.akshayravish@gmail.com>
2021-06-01 15:27:49 +0530

    add starting variable

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const header = document.getElementById('header');
+const title = document.getElementById('title');
+const excerpt = document.getElementById('excerpt');
+const profileImg = document.getElementById('profile-img');
+const name = document.getElementById('name');
+const date = document.getElementById('date');
 
+const animatedBg = document.querySelectorAll('.animated-bg');
+const animatedBgText = document.querySelectorAll('.animated-bg-text');
