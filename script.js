df2e8809bf7e5211d3d580f46cc05dd037de026e
akyravish <info.akshayravish@gmail.com>
2021-06-01 19:58:00 +0530

    show the fetch data on the placeholder

--- script.js
+++ script.js
@@ -2,31 +2,30 @@ const header = document.getElementById('header');
 const title = document.getElementById('title');
 const excerpt = document.getElementById('excerpt');
 const profileImg = document.getElementById('profile-img');
-const name = document.getElementById('name');
+const nameFull = document.getElementById('name');
 const date = document.getElementById('date');
 
 const animatedBg = document.querySelectorAll('.animated-bg');
 const animatedBgText = document.querySelectorAll('.animated-bg-text');
+const randomPic = 'https://source.unsplash.com/random/800x600';
 
 async function randomUser() {
 	const res = await fetch('https://randomuser.me/api/');
 	const data = await res.json();
 	const user = await data.results[0];
-	const mainPic = `https://source.unsplash.com/random/1280x720`;
-	console.log(mainPic);
 	setTimeout(() => {
-		showLoading(user, mainPic);
+		showLoading(user);
 	}, 1500);
 }
 
-const showLoading = (user, pic) => {
+const showLoading = (user) => {
 	header.innerHTML = `<img
-        src="${pic}"
+        src= "${randomPic}"
         alt="">`;
 	title.innerHTML = `Lorem ipsum dolor sit amet.`;
 	excerpt.innerHTML = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, itaque?`;
-	profileImg.innerHTML = ` <img src="https://randomuser.me/api/portraits/thumb/men/75.jpg" alt="">`;
-	name.innerHTML = `Jonny Steve`;
+	profileImg.innerHTML = ` <img src="${user.picture.thumbnail}" alt="">`;
+	nameFull.innerHTML = `${user.name.first}`;
 	date.innerHTML = `May 16, 2021`;
 
 	animatedBg.forEach((bg) => bg.classList.remove('animated-bg'));
