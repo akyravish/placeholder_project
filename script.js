d9b60cee20bf1573b128c93f63d157875dc7119c
akyravish <info.akshayravish@gmail.com>
2021-06-01 15:58:37 +0530

    add js code for the project

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ const date = document.getElementById('date');
 
 const animatedBg = document.querySelectorAll('.animated-bg');
 const animatedBgText = document.querySelectorAll('.animated-bg-text');
+
+setTimeout(() => {
+	header.innerHTML = `<img
+        src="https://images.unsplash.com/photo-1621864980675-4d7f27420a60?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=600&ixlib=rb-1.2.1&q=80&w=800"
+        alt="">`;
+	title.innerHTML = `Lorem ipsum dolor sit amet.`;
+	excerpt.innerHTML = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, itaque?`;
+	profileImg.innerHTML = ` <img src="https://randomuser.me/api/portraits/thumb/men/75.jpg" alt="">`;
+	name.innerHTML = `Jonny Steve`;
+	date.innerHTML = `May 16, 2021`;
+
+	animatedBg.forEach((bg) => bg.classList.remove('animated-bg'));
+	animatedBgText.forEach((bgtext) =>
+		bgtext.classList.remove('animated-bg-text')
+	);
+}, 2500);
